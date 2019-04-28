46ac8c3c218c37bee7308f58478dc5663f579694
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-28 12:03:34 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,9 +1,12 @@
 require "pry"
 class Hash
   def keys_of(*arguments)
+    matches = []
     arguments.collect do 
       |arg|
+      matches.push(keys[values.index(arg)])
       binding.pry
     end
+    matches
   end
 end
