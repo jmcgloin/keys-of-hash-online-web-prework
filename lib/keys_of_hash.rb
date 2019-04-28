3cd0e57b91f45be90cbccc2b2bc315b005c3d9d2
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-28 12:11:48 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    matches = []
+    values.each_index do
+      |ind|
+      val = values[ind]
+      key = keys[ind]
+      if arguments.index(val) != nil then matches.push(key) end
+    end
+    matches
   end
 end
